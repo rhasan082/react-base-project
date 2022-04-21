20521050588d2583bfc9dd75f21249cbfca3ca0d
Rakib <rhasan082@gmail.com>
2022-04-22 02:33:04 +0600

    fix babel config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   mode: "jit",
-  content: ["./src/*.{html,js, ts}", "./src/**/*.{js, ts}"],
+  content: [
+    "./src/*.{html,js,jsx,ts,tsx,css}",
+    "./src/**/*.{js,jsx,ts,tsx,css}",
+  ],
   theme: {
     extend: {},
   },
